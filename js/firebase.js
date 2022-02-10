b5438501a745d42145d10b5363e19285ae007f78
kenan1202 <kenan.tanriverdi.1@gmail.com>
2022-02-10 12:25:24 +0400

    Join part added

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
 
-// import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js/auth";
-
 import {
   getDatabase,
   set,
@@ -10,19 +8,49 @@ import {
   push
 } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-database.js";
 
+// import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js/auth";
+
+// import {
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+//   onAuthStateChanged,
+//   signOut,
+// } from "firebase/auth";
+
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  getAuth
+} from "https://www.gstatic.com/firebasejs/9.6.5/firebase-auth.js";
+
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBGu-1n5YB7rDMApzpLoD990eLnLUQpXCM",
+//   authDomain: "admin-panel-c56c3.firebaseapp.com",
+//   databaseURL: "https://admin-panel-c56c3-default-rtdb.firebaseio.com",
+//   projectId: "admin-panel-c56c3",
+//   storageBucket: "admin-panel-c56c3.appspot.com",
+//   messagingSenderId: "440643220813",
+//   appId: "1:440643220813:web:b456a2a01150e34ba97304"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBGu-1n5YB7rDMApzpLoD990eLnLUQpXCM",
-  authDomain: "admin-panel-c56c3.firebaseapp.com",
-  databaseURL: "https://admin-panel-c56c3-default-rtdb.firebaseio.com",
-  projectId: "admin-panel-c56c3",
-  storageBucket: "admin-panel-c56c3.appspot.com",
-  messagingSenderId: "440643220813",
-  appId: "1:440643220813:web:b456a2a01150e34ba97304"
+  apiKey: "AIzaSyDbnuogRcsXXb89D3QvfJyB728QbpzR8u4",
+  authDomain: "fir-9-project-6eb86.firebaseapp.com",
+  projectId: "fir-9-project-6eb86",
+  storageBucket: "fir-9-project-6eb86.appspot.com",
+  messagingSenderId: "629719105017",
+  appId: "1:629719105017:web:21c052c06d97be72c19128"
 };
+
 
 
 const app = initializeApp(firebaseConfig);
 
 const db = getDatabase(app);
 
-export { db, set, ref, onValue, push };
+const auth = getAuth(app);
+
+export { db, set, ref, onValue, push, createUserWithEmailAndPassword, onAuthStateChanged, auth };
--- js/join.js
+++ js/join.js
@@ -1,23 +1,42 @@
-import {  db, set, ref, onValue, push } from "./firebase.js";
-
-//Join part
-const fullName = $("#join-fullname");
-const email = $("#join-email");
-const joinBtn = $("#join-btn");
-// const alertSuccess = $('.alert-success');
-// const alertDanger = $('.alert-danger');
+import { createUserWithEmailAndPassword, auth, onAuthStateChanged } from "./firebase.js";
 
 
+// $('#joinLink').on('click', function() {
+//   $(".modal-backdrop").attr("class", "modal-backdrop modal fade")
+// });
 
-$("#myModal #join-form").on("submit", function (e) {
+async function registerUser() {
+  try {
+    const joinEmail = $("#join-email").val();
+    const joinPassword = $("#join-password").val();
+
+
+    await createUserWithEmailAndPassword(
+      auth,
+      joinEmail,
+      joinPassword
+    );
+
+
+    
+    $('#join-email').val('');
+    $('#join-password').val('');
+
+    $('.alert-danger').css({ display: 'none'})
+
+    $("#myModal").attr("class", "modal fade").css("display", "none")
+    $(".modal-backdrop").attr("class", "modal-backdrop modal fade")
+    
+  } catch (error) {
+    $('.alert-danger').show();
+    $('.alert-danger .error-message').text(error.message);
+  }
+}
+
+
+
+$("#join-form").on("submit", function(e) {
   e.preventDefault();
-  const joinUserName = $("#myModal #join-form #join-fullname").val();
-  const joinEmail = $("#myModal #join-form #join-email").val();
-  const pushJoinInfo = push(ref(db, "/book-store/users"))
-  set(pushJoinInfo, {
-    fullname: joinUserName,
-    email: joinEmail
-  })
-  $("#myModal").attr("class", "modal fade").css("display", "none")
-  $(".modal-backdrop").attr("class", "modal-backdrop modal fade")
-})
+
+  registerUser();
+});
